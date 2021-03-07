c147dedfedc903fddd58950d0c2a5c84c8a5fa91
MegMcGregor <megmcgregor1@gmail.com>
2021-03-06 21:07:15 -0600

    commented out filter

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,29 +1,16 @@
 import { getFish } from "./fishData.js";
 import { Fish } from "./Fish.js";
-import { mostHolyFish } from "./fishFilter";
-import { soldierFish } from "./fishFilter";
-import { unworthyFish } from "./fishFilter";
+// import { mostHolyFish } from "./fishFilter";
+// import { soldierFish } from "./fishFilter";
+// import { unworthyFish } from "./fishFilter";
 
 
 export const fishList = () => {
-    const holyFishes = mostHolyFish();
-    addFishToDom(holyFishes);
 
-    const soldierFishes = soldierFish();
-    addFishToDom(soldierFishes);
-
-    const unworthyFishes = unworthyFish();
-    addFishToDom(unworthyFishes)
-    // const allFishes = holyFishes.concat(soldierFishes, unworthyFishes);
-}
-
-const addFishToDom = (fishArray) => {
-
+    const allFishes = getFish()
     const DOMLocation = document.querySelector("#fishList");
     let fishHTMLRepresentations = "";
-
-    for (const oneThingFromTheSea of fishArray) {
-
+    for (const oneThingFromTheSea of allFishes) {
         fishHTMLRepresentations += Fish(oneThingFromTheSea);
     }
 
--- scripts/fishFilter.js
+++ scripts/fishFilter.js
@@ -1,36 +1,36 @@
 
-import { fishCollection } from "./fishData.js";
-import { getFish } from "./fishData";
+// import { fishCollection } from "./fishData.js";
+// import { getFish } from "./fishData";
 
-export const mostHolyFish = () => {
-    const holyFish = []
+// export const mostHolyFish = () => {
+//     const holyFish = []
 
-    for (const oneFish of fishCollection) {
-        if (oneFish.inches % 3 === 0) {
-            holyFish.push(oneFish)
-        }
-    }
-    return holyFish
-}
+//     for (const oneFish of fishCollection) {
+//         if (oneFish.inches % 3 === 0) {
+//             holyFish.push(oneFish)
+//         }
+//     }
+//     return holyFish
+// }
 
-export const soldierFish = () => {
-    const soldierFish = []
+// export const soldierFish = () => {
+//     const soldierFish = []
 
-    for (const oneFish of fishCollection) {
-        if (oneFish.inches % 5 === 0) {
-            soldierFish.push(oneFish)
-        }
-    }
-    return soldierFish
-}
+//     for (const oneFish of fishCollection) {
+//         if (oneFish.inches % 5 === 0) {
+//             soldierFish.push(oneFish)
+//         }
+//     }
+//     return soldierFish
+// }
 
-export const unworthyFish = () => {
-    const unworthyFish = []
+// export const unworthyFish = () => {
+//     const unworthyFish = []
 
-    for (const oneFish of unworthyFish) {
-        if (oneFish % 3 || 5 !== 0) {
-            unworthyFish.push(unworthyFish)
-        }
-    }
-    return unworthyFish
-}
+//     for (const oneFish of unworthyFish) {
+//         if (oneFish % 3 || 5 !== 0) {
+//             unworthyFish.push(unworthyFish)
+//         }
+//     }
+//     return unworthyFish
+// }
